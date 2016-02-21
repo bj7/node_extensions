c0cc020a746b064ce976d2a969cbef96314cd08c
Josh Bernitt <jjbernitt@gmail.com>
2016-02-21 18:10:44 -0500

    Hello world
    Hello world example of a C++ nodeJS extension.

---
+++ build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/addon/hello.o := c++ '-DNODE_GYP_MODULE_NAME=addon' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/joshuabernitt/.node-gyp/4.2.6/include/node -I/Users/joshuabernitt/.node-gyp/4.2.6/src -I/Users/joshuabernitt/.node-gyp/4.2.6/deps/uv/include -I/Users/joshuabernitt/.node-gyp/4.2.6/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c -o Release/obj.target/addon/hello.o ../hello.cc
+Release/obj.target/addon/hello.o: ../hello.cc \
+  /Users/joshuabernitt/.node-gyp/4.2.6/include/node/node.h \
+  /Users/joshuabernitt/.node-gyp/4.2.6/include/node/v8.h \
+  /Users/joshuabernitt/.node-gyp/4.2.6/include/node/v8-version.h \
+  /Users/joshuabernitt/.node-gyp/4.2.6/include/node/v8config.h \
+  /Users/joshuabernitt/.node-gyp/4.2.6/include/node/node_version.h
+../hello.cc:
+/Users/joshuabernitt/.node-gyp/4.2.6/include/node/node.h:
+/Users/joshuabernitt/.node-gyp/4.2.6/include/node/v8.h:
+/Users/joshuabernitt/.node-gyp/4.2.6/include/node/v8-version.h:
+/Users/joshuabernitt/.node-gyp/4.2.6/include/node/v8config.h:
+/Users/joshuabernitt/.node-gyp/4.2.6/include/node/node_version.h:
